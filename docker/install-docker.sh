00cbf7b6498a5f4e978751a719ba62cfc5418960
zjb0807 <zjb0807@qq.com>
2018-06-07 14:57:46 +0800

    fix ubuntu dash

--- docker/install-docker.sh
+++ docker/install-docker.sh
@@ -2,23 +2,23 @@
 
 # install bityuan node
 docker pull bityuan/node:latest
-docker run -d --name "bty" -p 13802:13802 -v /data:/data --restart always bityuan/node:latest
+docker run -d --name bty -p 13802:13802 -v /data:/data --restart always bityuan/node:latest
 
-for((i=1;i<=30;i++));  
+for i in $(seq 30)
 do
-    num=$(docker ps -a | grep '\bbty\b' |grep -F '13802'|wc -l)
-    echo "bty num:" $num
-    if [ $num -eq 1 ]; then
+    num=$(docker ps -a | grep '\bbty\b' |grep -c -F '13802')
+    echo "bty num: $num"
+    if [ "$num" -eq 1 ]; then
     	break
     fi
     sleep 1s
 done 
 
-for((i=1;i<=30;i++));
+for i in $(seq 30)
 do
-    peersNum=$(docker exec -it bty chain33-cli net peer_info | grep addr | wc -l)
-    if [ $peersNum -gt 1 ]; then
-    	echo "peersNum:" $peersNum
+    peersNum=$(docker exec -it bty chain33-cli net peer_info | grep addr -c)
+    if [ "$peersNum" -gt 1 ]; then
+    	echo "peersNum: $peersNum"
         break
     fi
     sleep 1s
@@ -32,20 +32,21 @@ echo "seed:$seed"
 docker exec -it bty /bin/bash -c "chain33-cli seed save -p 123456 -s $seed"
 docker exec -it bty chain33-cli wallet unlock -p 123456 -s wallet -t 0
 
-for((i=1;i<=30;i++));
+for i in $(seq 30)
 do
-    num=$(docker exec -it bty chain33-cli account  list  | grep "node award" | wc -l)
-    echo "addr num:" $num
-    if [ $num -gt 0 ]; then 
+    num=$(docker exec -it bty chain33-cli account  list  | grep "node award" -c)
+    echo "addr num: $num"
+    if [ "$num" -gt 0 ]; then 
     	node=$(docker exec -it bty chain33-cli account  list  | grep "node award" -B 2 | grep addr | cut -d '"' -f 4)
-	echo $node
+	echo "$node"
 	break
     fi
     sleep 1s
 done
+echo "$i"
 
 #echo $node
-priv=$(docker exec -it bty chain33-cli  account dump_key -a $node | grep replystr | cut -d '"' -f 4)
+priv=$(docker exec -it bty chain33-cli  account dump_key -a "$node" | grep replystr | cut -d '"' -f 4)
 #echo $priv
 echo "Run the cmd in wallet PC: account import_key -l ali00001 -k $priv"
 
--- docker/install.sh
+++ docker/install.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 set -e
 curl -fsSL https://get.daocloud.io/docker | sh
+
+systemctl enable docker.service
+systemctl start  docker
+
 curl -fsSL https://raw.githubusercontent.com/bityuan/install-script/master/docker/install-docker.sh | sh
+
