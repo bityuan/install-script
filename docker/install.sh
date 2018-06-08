d4d7f8cfc4684c250b09331466f36a67e27c6ce5
zjb0807 <zjb0807@qq.com>
2018-06-08 11:33:31 +0800

    add chain33-cli script

--- docker/install.sh
+++ docker/install.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 set -e
 
+# install docker
 curl -fsSL https://get.daocloud.io/docker | sh
-curl -fsSL https://raw.githubusercontent.com/bityuan/install-script/master/docker/install-docker.sh | sh
 
+# create script to run chain33-cli
+echo "#!/bin/bash" > /usr/local/bin/chain33-cli
+echo "set -x" > /usr/local/bin/chain33-cli
+echo 'docker exec -it bty chain33-cli $*' >> /usr/local/bin/chain33-cli
+chmod 0777 /usr/local/bin/chain33-cli
+
+# install bityuan node && run 
+curl -fsSL https://raw.githubusercontent.com/bityuan/install-script/master/docker/install-docker.sh | sh
